21706d8d364362df2dd299b4ed663e125fadf6b3
augustocolo <33198795+augustocolo@users.noreply.github.com>
2017-12-02 15:29:55 +0100

    Entry points - compiled

--- Studies/EntryPoints/main.c
+++ Studies/EntryPoints/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define MAXPOW 25
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *fp;
+    fp=fopen("collatzEntryPoints.csv","w");
+    int n=0;
+    unsigned int power, div;
+    for (n=0; n<MAXPOW; n++){
+        power = (unsigned int) pow(2, n);
+        switch ((power - 1) % 3) {
+            case 0:
+                div = (power - 1) / 3;
+                fprintf(fp, "%d;%d;%d\n", n, power, div);
+                break;
+            default:
+                fprintf(fp, "%d;%d;%d\n", n, power, 0);
+                break;
+        }
+    }
+
+    fclose(fp);
     return 0;
 }
